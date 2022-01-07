677f3d5bfa0a79b8185dfa9c15b95cea5bed79a6
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-07 10:25:30 +0530

    Added : Program for the Create 52 unique cards

--- src/com/blz/workshop/CardsPlay.java
+++ src/com/blz/workshop/CardsPlay.java
@@ -1,6 +1,31 @@
 package com.blz.workshop;
+
+import java.util.ArrayList;
+
 public class CardsPlay {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Deck of Cards !!!");
+
+	String[] cards = new String[52];
+	ArrayList<String> cardsArr = new ArrayList<>();
+
+	public void generateCards() {
+
+		String[] suit = { "Clubs", "Diamonds", "Hearts", "Spades" };
+		String[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+		// get combination
+		for (int i = 0; i < suit.length; i++) {
+			for (int j = 0; j < rank.length; j++) {
+
+				cardsArr.add(suit[i] + rank[j]);
+			}
+		}
 	}
+
+	public void printCards() {
+
+		System.out.print("Cards : ");
+		for (int i = 0; i < cardsArr.size(); i++) {
+			System.out.print(" " + cardsArr.get(i) + "\n");
+		}
+	}
+
 }
--- src/com/blz/workshop/Main.java
+++ src/com/blz/workshop/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		CardsPlay play = new CardsPlay();
+		play.generateCards();
+		play.printCards();
 
 	}
 }
