0ea9d5df0c0eca2b7071c1d0362afd88fc62b181
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-07 11:02:21 +0530

    Added : Program for the Add Player into the game

--- src/com/blz/workshop/CardsPlay.java
+++ src/com/blz/workshop/CardsPlay.java
@@ -1,11 +1,17 @@
 package com.blz.workshop;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CardsPlay {
-
+	int playerCount;
 	String[] cards = new String[52];
+	ArrayList<Player> playerList = new ArrayList<>();
 	ArrayList<String> cardsArr = new ArrayList<>();
+
+	public CardsPlay(int playerCount) {
+		this.playerCount = playerCount;
+	}
 
 	public void generateCards() {
 
@@ -25,6 +31,23 @@ public class CardsPlay {
 		System.out.print("Cards : ");
 		for (int i = 0; i < cardsArr.size(); i++) {
 			System.out.print(" " + cardsArr.get(i) + "\n");
+		}
+	}
+
+	public void addPlayer() {
+
+		Scanner sc = new Scanner(System.in);
+
+		if (playerCount >= 2 && playerCount <= 4) {
+			for (int i = 1; i <= playerCount; i++) {
+				System.out.print(" Enter player " + i + " name : ");
+				String name = sc.nextLine();
+				Player player1 = new Player(name);
+				playerList.add(player1);
+
+			}
+		} else {
+			System.out.println("Invalid input # Enter between 2 to 4 #");
 		}
 	}
 
--- src/com/blz/workshop/Main.java
+++ src/com/blz/workshop/Main.java
@@ -1,12 +1,18 @@
 package com.blz.workshop;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		CardsPlay play = new CardsPlay();
+		Scanner sc = new Scanner(System.in);
+		System.out.print(" Enter number of players :");
+		int playerCount = sc.nextInt();
+
+		CardsPlay play = new CardsPlay(playerCount);
 		play.generateCards();
-		play.printCards();
+		play.addPlayer();
 
 	}
 }
